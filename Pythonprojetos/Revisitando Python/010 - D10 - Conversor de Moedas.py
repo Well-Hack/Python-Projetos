f924e1c0dc2b7014fdca713160adf391aa3fc61e
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-08 17:00:42 -0300

    Update 010 - D10 - Conversor de Moedas.py
    Conversor de moeda

--- Pythonprojetos/Revisitando Python/010 - D10 - Conversor de Moedas.py
+++ Pythonprojetos/Revisitando Python/010 - D10 - Conversor de Moedas.py
@@ -1,5 +1,11 @@
-print('Desafio 9 - Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.')
+print('Desafio 10 - Crie um programa que leia quanto dinheiro uma pessoa tem na carteira R$ e mostre quantos dólares ela pode comprar. ')
 
 print('=*'*30)
-print('Tabuada')
+print('Conversor em Dólares')
 print('=*'*30)
+
+
+r1 = float(input('Por gentileza, digite o valor em Reais(BRL), do quanto voce quer converter para dólares: '))
+d1 = r1 / 5.14
+
+print('Voce pode comprar {:.2f} em Dólares (USD) com esse valor em {:.2f} Reais (BRL)'.format(d1,r1))
