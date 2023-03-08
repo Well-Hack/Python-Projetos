74454e3ddd4b0e3e4443aea4094d7a4a5a3b9e3c
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-07 21:06:58 -0300

    Desafio-3
    somando dois numeros

---
+++ Pythonprojetos/Revisitando Python/001 - Desafio 1.py
@@ -0,0 +1,2 @@
+n1 = str(input('Digite seu nome: '))
+print(f'Seu nome é {n1}')
---
+++ Pythonprojetos/Revisitando Python/003 - D3 - Somando dois numeros.py
@@ -0,0 +1,11 @@
+print('Desafio 3 - Crie um programa que leia dois numeros e soma entre eles')
+
+print('=*'*30)
+print('Somando dois números')
+print('=*'*30)
+
+nome = str(input('Por favor, digite sue nome: '))
+n1 = int(input('Agora, vou pedir a gentileza que digite o primeiro número: '))
+n2 = int(input('Agora digita o segundo numero, pois iremos somar!: '))
+s = n1 + n2
+print(f'Caro(a) {nome}, você digitou  {n1} e depois digitou {n2}, então a soma dos dois é {s}')
