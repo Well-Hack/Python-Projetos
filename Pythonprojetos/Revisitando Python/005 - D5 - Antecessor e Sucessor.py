8b7b99520630079feeea3023e89f38542fae6367
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-07 21:17:31 -0300

    Update 005 - D5 - Antecessor e Sucessor.py
    desafio 5 revisitado!!

--- Pythonprojetos/Revisitando Python/005 - D5 - Antecessor e Sucessor.py
+++ Pythonprojetos/Revisitando Python/005 - D5 - Antecessor e Sucessor.py
@@ -4,3 +4,8 @@ print('=*'*30)
 print('Antecessor e Sucessor')
 print('=*'*30)
 
+s1 = int(input('Por favor, digite um numero que iremos mostrar seu antecessor e o seu sucessor!: '))
+s2 = s1 - 1
+s3 = s1 + 1
+
+print(f'Pois então, você digitou o numero {s1} e o seu antecessor é {s2} e o seu sucessor è {s3}')
