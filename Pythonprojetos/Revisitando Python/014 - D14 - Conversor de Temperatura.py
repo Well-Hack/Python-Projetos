79ecf0789fcbbe2910f7a34b0ffd5fe4e59692b1
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-09 16:19:03 -0300

    Create 014 - D14 - Conversor de Temperatura.py
    revisitando desafio 14

---
+++ Pythonprojetos/Revisitando Python/014 - D14 - Conversor de Temperatura.py
@@ -0,0 +1,10 @@
+print('Desafio 13 - Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.')
+
+print('=*' * 30)
+print('Conversor de temperatura')
+print('=*' * 30)
+
+
+c = float(input('Informe a temperatura em °C: '))
+f = 9 * c / 5 + 32
+print(f'A temperatura em °C {c} convertido para °F fica °F {f}')
