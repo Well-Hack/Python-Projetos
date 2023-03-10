f00e59122410616fe894c94c1aaa46eae1ec8b04
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-10 15:15:38 -0300

    Desafio 25
    Desafio 25 revisitado

---
+++ Pythonprojetos/Revisitando Python/025 - D25 - Procurando uma string dentro de outra.py
@@ -0,0 +1,10 @@
+print('Desafio 24 - Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.')
+
+print('=*' * 30)
+print('Procurando uma string dentro de outra')
+print('=*' * 30)
+
+nome = str(input('Digite seu nome completo, e diremos se tem SILVA em seu nome: ')).upper().strip()
+nome2 = 'SILVA' in nome
+print(f'seu nome tem silva? {nome2}')
+
