5ef2e0b409a33d6bb576c994783541d31d1ab520
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-11 20:33:27 -0300

    desafio 74

--- Pythonprojetos/exercicio - 73 Tuplas com time de Futebol.py
+++ Pythonprojetos/exercicio - 73 Tuplas com time de Futebol.py
@@ -1,3 +1,34 @@
-print('Exercicio - 73 Crie um programa que simule um caixa eletronico. No inicio, pergunte ao usuario \n'
-      'qual valor sera sacado (numeros inteiros) e o programa vai informar quantas cédulas de cada \n valor serão'
-      ' entregues valores das cedulas: 1,10,20,50')
+print('Exercicio - 73 Crie uma tupla preenchida com os 20 primeiros \n'
+      'colocados da Tabela do Campeonato Brasileiro de Futebol, \n'
+      'na ordem de colocação. Depois mostre:')
+
+equipes =('Red Bull Racing', 'Aston Martin', 'Mercedes F1 Team', 'Scuderia Ferrari', 'Alfa Romeo', 'Alpine F1 Team', 'Williams Racing',
+        'Scuderia Alpha Tauri', 'Haas F1 Team', 'Mclaren Racing')
+
+
+
+print('=+'*30)
+print('Informação da Formula 1')
+print('=+'*30)
+
+print('Escolha uma das opções a baixo: \n'
+      '[1] Para ver os Três primeiros colocados \n'
+      '[2] Para ver os Três Ultimos colocados \n'
+      '[3] Em ordem Alfabética. \n'
+      '[4] Buscar A Alfa Romeo na Classificação \n'
+      'Ou qualquer outro opção para Encerrar o Programa \n')
+while True:
+    opcao = int(input('Qual você escolhe: '))
+    if opcao == 1:
+     print(equipes[:3])
+    elif opcao == 2:
+        print(equipes[7:])
+    elif opcao == 3:
+        print(sorted(equipes))
+    elif opcao == 4:
+        print(equipes.index('Alfa Romeo')+1)
+    else:
+        break
+print('Obrigado')
+
+
---
+++ Pythonprojetos/exercicio - 74 - Sorteando o Maior e o menor valor.py
@@ -0,0 +1,13 @@
+from random import randint
+print('Exercicio - 74 Crie um programa que vai gerar cinco '
+      'números aleatórios e colocar em uma tupla. Depois disso, \n '
+      'mostre a listagem de números gerados e também indique o \n'
+      ' menor e o maior valor que estão na tupla.')
+
+print('=+'*30)
+print('Maior e menor valores em Tupla')
+print('=+'*30)
+
+
+n1 = randint('', '','', '', '')
+print(n1)
