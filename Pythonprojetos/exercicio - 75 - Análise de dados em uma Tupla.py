9fe5f03cbdfa2b7b63b235480d5375ea5277a190
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-12 13:01:27 -0300

    Create exercicio - 75 - Análise de dados em uma Tupla.py
    Desafio 75

---
+++ Pythonprojetos/exercicio - 75 - Análise de dados em uma Tupla.py
@@ -0,0 +1,19 @@
+print('Exercicio - 75 esenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:'
+
+'A) Quantas vezes apareceu o valor 9.'
+
+'B) Em que posição foi digitado o primeiro valor 3.'
+
+'C) Quais foram os números pares.')
+
+print('=+'*30)
+print('Análise de dados em uma Tupla')
+print('=+'*30)
+
+n1 = (int(input('Digite um numero: ')),
+      int(input('Digite outro numero: ')),
+      int(input('Digite mais um numero: ')),
+      int(input('Digite o ultimo numero: ')))
+
+print(f'Voce digitou os valores: {n1}')
+print(f'O valor 9 apareceu {n1.count(9)} vezes')
