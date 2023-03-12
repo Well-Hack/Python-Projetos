05cbcd9950ef597ddf43e7d3290087b145fe4654
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-12 17:06:13 -0300

    Update Listas Aprendendo.py

---
+++ Pythonprojetos/Listas Aprendendo.py
@@ -0,0 +1,5 @@
+#var.append() ADICIONA elementos na lista
+#var.insert(n,'') INSERE elementos em uma lista em uma posição descrita
+#del var[] Realiza deleção de um elemento em uma lista
+#var.sort() Realiza a ordenação dos elementos
+#var.sort(reverse=True)
