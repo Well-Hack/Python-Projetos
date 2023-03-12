20ce52faaa4e67ef1bbc8d1ada17d50490855262
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-12 13:07:46 -0300

    Update exercicio - 75 - Análise de dados em uma Tupla.py

--- Pythonprojetos/exercicio - 75 - Análise de dados em uma Tupla.py
+++ Pythonprojetos/exercicio - 75 - Análise de dados em uma Tupla.py
@@ -17,3 +17,9 @@ n1 = (int(input('Digite um numero: ')),
 
 print(f'Voce digitou os valores: {n1}')
 print(f'O valor 9 apareceu {n1.count(9)} vezes')
+print(f'O valor 3 apareceu na {n1.index(3)+1}ª posição')
+print(f'Os valores pares, digitados foram',end='')
+
+for n in n1:
+    if n % 2 == 0:
+        print(n, end=' ')
