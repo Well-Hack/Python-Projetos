7f8e3552839e7fc2a7945e4552034fb1b6ecaa00
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-12 18:30:27 -0300

    Update exercicio - 79 Valores Unicos em uma lista.py
    Valores unicos em uma lista

--- Pythonprojetos/exercicio - 79 Valores Unicos em uma lista.py
+++ Pythonprojetos/exercicio - 79 Valores Unicos em uma lista.py
@@ -1,7 +1,22 @@
-print('Exercicio - 79 Faça um programa que leia 5 valores numéricos e \n '
-      'guarde-os em uma lista. No final, mostre qual foi o maior e o \n'
-      ' menor valor digitado e as suas respectivas posições na lista.')
+print('Exercicio - 79 Crie um programa onde o usuário possa digitar vários \n'
+      ' valores numéricos e cadastre-os em uma lista. Caso o número já exista lá dentro, \n '
+      'ele não será adicionado. No final, serão exibidos todos os valores únicos digitados, \n'
+      ' em ordem crescente.')
 
 print('=+'*30)
-print('Maior e menor valores em Listas')
+print('Valores unicos em uma lista')
 print('=+'*30)
+
+n = list()
+while True:
+      n1 = int(input(f'Digite um valor: '))
+      if n1 not in n:
+            n.append(n1)
+            print('Valor Adicionado com Sucesso!')
+      else:
+            print('Valor duplicado: Não vou adicionar')
+      r = str(input('Quer Continuar? [S/N]: ')).upper()
+      if r == 'N':
+            break
+n.sort()
+print(f'O valores digitados foram: {n}')
