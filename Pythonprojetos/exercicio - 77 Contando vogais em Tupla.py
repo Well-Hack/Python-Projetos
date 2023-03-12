687ecf95a72b484489a11370267b9b5eccf3951b
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-12 13:53:13 -0300

    Create exercicio - 77 Contando vogais em Tupla.py
    Desafio 77

---
+++ Pythonprojetos/exercicio - 77 Contando vogais em Tupla.py
@@ -0,0 +1,12 @@
+print('Exercicio - 77 Crie um programa que tenha uma tupla com várias palavras \n'
+      ' (não usar acentos). Depois disso, você deve mostrar, \n'
+      'para cada palavra, quais são as suas vogais.')
+
+print('=+'*20)
+print('Contando vogais em Tuplas')
+print('=+'*20)
+
+frase = ('Maca', 'Abacaxi', 'Banana', 'Tomate', 'Limao', 'Maracuja')
+print(frase)
+
+print(f'A palavra {frase[0]} tem as vogais')
