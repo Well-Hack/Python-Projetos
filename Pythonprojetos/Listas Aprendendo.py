575cfba57efb7e13b926cd57185b6aaf434cbe5d
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-12 17:10:06 -0300

    Update Listas Aprendendo.py

--- Pythonprojetos/Listas Aprendendo.py
+++ Pythonprojetos/Listas Aprendendo.py
@@ -3,3 +3,14 @@
 #del var[] Realiza deleção de um elemento em uma lista
 #var.sort() Realiza a ordenação dos elementos
 #var.sort(reverse=True)
+
+num = [2,5,9,1]
+print(num)
+num.insert(2, 22)
+print(num)
+num.sort()
+print(num)
+num.sort(reverse=True)
+print(num)
+del num[2]
+print(num)
