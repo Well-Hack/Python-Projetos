09140767ab0940028f62b7ea5347d3a34bd13475
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-12 17:54:40 -0300

    Create exercicio - 79 Valores Unicos em uma lista.py

---
+++ Pythonprojetos/exercicio - 79 Valores Unicos em uma lista.py
@@ -0,0 +1,7 @@
+print('Exercicio - 79 Faça um programa que leia 5 valores numéricos e \n '
+      'guarde-os em uma lista. No final, mostre qual foi o maior e o \n'
+      ' menor valor digitado e as suas respectivas posições na lista.')
+
+print('=+'*30)
+print('Maior e menor valores em Listas')
+print('=+'*30)
