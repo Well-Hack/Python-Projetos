5b137f6bab88fdc1ef450d73a126622bed01493e
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-13 16:10:37 -0300

    Create exercicio - 83 Validando expressões matematicas.py

---
+++ Pythonprojetos/exercicio - 83 Validando expressões matematicas.py
@@ -0,0 +1,8 @@
+print('Exercicio - 83  Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. \n'
+      ' Seu aplicativo deverá analisar se a expressão passada está \n'
+      'com os parênteses abertos e fechados na ordem correta.')
+
+print('=+'*30)
+print('DValidando expressões matemáticas')
+print('=+'*30)
+
