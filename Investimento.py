7f858a5dba9061025da2ee0d8724001d1b919562
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-13 16:32:57 -0300

    Create Investimento.py

---
+++ Investimento.py
@@ -0,0 +1,10 @@
+print('+='*30)
+print('Calculo de Investimento')
+print('+='*30)
+
+total = float(input('Digite o valor que voce vai querer investir para resgatar futuramente: '))
+anof = int(input('Digite a daqui a quantos anos você ira querer resgatar esse valor: '))
+vlri = float(input('O quanto voce ira investir mensal para resgatar esse valor? '))
+anom = anof * 12
+
+print(f'Você tem {anom} meses para resgatar esse valor')
