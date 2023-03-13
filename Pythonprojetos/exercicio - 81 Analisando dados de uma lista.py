724bf13cf3b3e1301db35ba0d4a3f4527cfdc915
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-12 21:39:05 -0300

    Create exercicio - 81 Analisando dados de uma lista.py

---
+++ Pythonprojetos/exercicio - 81 Analisando dados de uma lista.py
@@ -0,0 +1,27 @@
+print('Exercicio - 81 Crie um programa que vai ler vários números e colocar em uma lista.\n '
+      ' Depois disso, mostre:\n'
+      ' A) Quantos números foram digitados. \n'
+      ' B) A lista de valores, ordenada de forma decrescente. \n'
+      ' C) Se o valor 5 foi digitado e está ou não na lista.')
+
+print('=+'*30)
+print('Analisando dados de uma lista')
+print('=+'*30)
+
+list = []
+c = 0
+while True:
+    list.append(int(input('Digite um valor: ')))
+    r = str(input('Deseja colcoar mais um valor? [S/N]')).upper()
+    c += 1
+    if r == 'N':
+        break
+print(f'Você digitou {c} elementos.')
+list.sort(reverse=True)
+print(f'Você digitou os numeros em forma decrescente {list}')
+if 5 in list:
+    print('O valor 5 faz parte da lista')
+else:
+    print('O valor 5 não faz parte da lista')
+
+
