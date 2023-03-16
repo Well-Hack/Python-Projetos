c45f9796fe275369a7aaf8934378e6d43e8de5c9
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-15 23:21:44 -0300

    matriz
    matriz

--- Pythonprojetos/exercicio - 86 Matriz em python.py
+++ Pythonprojetos/exercicio - 86 Matriz em python.py
@@ -5,3 +5,16 @@ print('Exercicio - 85  Crie um programa que declare uma matriz de \n'
 print('=+'*30)
 print('Matriz em Python')
 print('=+'*30)
+
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for l in range(3):
+    for c in range(3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}] '))
+print("="*40)
+for l in range(3):
+    for c in range(3):
+        print(f'[{matriz[l] [c]:^5}]', end='')
+    print()
+
+
