4eea9f4dae97d5d90c00243c6796e3e1568f00fd
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-03-15 23:13:26 -0300

    matriz em python

---
+++ Pythonprojetos/exercicio - 86 Matriz em python.py
@@ -0,0 +1,7 @@
+print('Exercicio - 85  Crie um programa que declare uma matriz de \n'
+      'dimensão 3×3 e preencha com valores lidos pelo teclado. \n'
+      'No final, mostre a matriz na tela, com a formatação correta. ')
+
+print('=+'*30)
+print('Matriz em Python')
+print('=+'*30)
--- exercicios - 85 Versão do Professor.py
+++ exercicios - 85 Versão do Professor.py
@@ -7,24 +7,20 @@ print('=+'*30)
 print('Lista composta e análise de dados')
 print('=+'*30)
 
-data = list()
-par = list()
-impar = list()
-print('Olá, digite sete numeros separados que informaremos os pares e os impares')
+
+num = [[],[]]
+valor = 0
 for c in range(7):
-        vlr = int(input(f'Digite o {c+1}º valor: '))
-        data.append(vlr)
-        if vlr % 2 == 0:
-            par.append(vlr)
-            print(f'Valor adicionado na lista de pares {par}')
-        else:
-            impar.append(vlr)
-            print(f'Valor foi adicionado na lista de impares {impar}')
-impar.sort()
-par.sort()
-print("=+"*50)
-print(f'Os numeros foram: {data}')
-print(f'Voce adicionou valores pares que foram: {par} ')
-print(f'Vocẽ adicionou valores impares que são: {impar}')
-print('Obrigado')
+    valor = int(input((f'Digite o {c+1}º valor: ')))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+num[0].sort()
+num[1].sort()
+print('=='*30)
+print(f'Todos os valores: {num}')
+print(f'Os valores ṕares são {num[0]}')
+print(f'Os valores impares são {num[1]}')
+
 
