a58e23e96d3c0037140912fb8fa0eb9d47fd6658
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-10-10 19:31:41 -0300

    Update exercicio - 57 validação de dados.py

--- exercicio - 57 validação de dados.py
+++ exercicio - 57 validação de dados.py
@@ -7,6 +7,6 @@ nome = str(input('Ola! Bem vindo!, Informe Por gentileza o seu nome completo: ')
 i = int(input('Ok \033[032:1m {} \033[m , agora vou pedir que voce digite a sua idade: '.format(nome)))
 s = str(input('Certo \033[032:1m {} \033[m, agora vou pedir que voce me informe o seu sexo, M para masculino ou F para Feminino: '.format(nome))).strip().upper()[0]
 while s != 'M' and 'F':
-    print('Desculpe, mas você é um alienigena? vamos tentar de novo')
+    print('Desculpe, digito invalido, tente novamente.')
     s = str(input('M para masculino ou F para Feminino: ')).upper
 cid = str(input('Muito bem, agora me diga a sua cidade: '))
