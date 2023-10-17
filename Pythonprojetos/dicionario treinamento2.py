bb8ca639d218f20ef91cafd8f785785eb599347c
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-10-17 19:05:27 -0300

    python
    commit

---
+++ Pythonprojetos/dicionario treinamento2.py
@@ -0,0 +1,3 @@
+pessoas = {'nome':'Wellerson', 'idade':27, 'Genero':'Masculino'}
+for k, v in pessoas.items():
+    print(f'{k} == {v}')
---
+++ Pythonprojetos/dicionarios treinamento.py
@@ -0,0 +1,10 @@
+filme = {
+    'titulo':'Star Wars',
+    'ano': '1997',
+    'genero': 'ação'
+    }
+
+print(filme['titulo'])
+print(filme.values())
+print(filme.keys())
+print(filme.items())
