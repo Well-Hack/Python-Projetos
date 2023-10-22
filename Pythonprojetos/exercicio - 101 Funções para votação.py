e0a7f50597789a74e6c92e42e5c64d91b6b16245
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-10-22 12:00:39 -0300

    Update exercicio - 101 Funções para votação.py

--- Pythonprojetos/exercicio - 101 Funções para votação.py
+++ Pythonprojetos/exercicio - 101 Funções para votação.py
@@ -1,1 +1,7 @@
-print('Exercicio 101 - Crie um programa')
+print('Exercicio 101 - Crie um programa que tenha \n'
+      'uma função chamado voto() que vai receber como \n'
+      'parametro o ano de nascimento de uma pessoa.\n Retornando'
+      'um valor literal indicando se uma pessoa tem voto \n'
+      'negado, opcional ou obrigatório nas eleições')
+
+print(Hello world)
