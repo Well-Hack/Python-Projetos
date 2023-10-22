053614fcd75093c140ef034ab648180e79dad0fc
Wellerson Ferreira Queiroz de Souza <107362025+Well-Hack@users.noreply.github.com>
2023-10-22 12:44:23 -0300

    Update exercicio - 102 Fatorial com função.py

---
+++ Pythonprojetos/exercicio - 102 Fatorial com função.py
@@ -0,0 +1,27 @@
+print('Exercicio 102 - Crie um programa que tenha uma função fatorial() que \n'
+      'receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, \n'
+      'que será um valor lógico (opcional) indicando se será mostrado ou não na tela\n'
+      'o processo de cálculo do fatorial.')
+
+def fatorial(n, show=False):
+    """
+    --> Calcula o Fatorial de um número.
+    :param n: O numero a ser calculado.
+    :param show:(opcional) Mostrar ou não a conta.
+    :return: O valor do Fatorial de um número n.
+    """
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(f'{c}', end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= c
+    return f
+
+
+
+#Programa principal
+print(fatorial(5, show=True))
